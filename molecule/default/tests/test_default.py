8fbcfa6863c6f6565c2b75dd41b28a51b618d512
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2024-01-03 20:29:40 +0100

    rearrange tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import testinfra.utils.ansible_runner
 import os
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_phpinfo(host):
-	cmd = host.run("curl -v http://127.0.0.1/index.html")
+def test_pingpong(host):
+	cmd = host.run("redis-cli -s /var/run/redis/default.sock ping")
 	print(cmd.stdout)
-	assert 'HTTP/1.1 200 OK' in cmd.stderr
-	assert "Success" in cmd.stdout
-	assert "The test page is displayed correctly" in cmd.stdout
+	assert "PONG" in cmd.stdout
